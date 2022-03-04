04861bddcde4d36871c965f1dfcc5a12e6e64a15
Vishal Kumar Singh <vishalsingh8574031285@gmail.com>
2022-03-04 18:34:09 +0530

    Updated Functionality

--- search_function.js
+++ search_function.js
@@ -1,39 +1,89 @@
-let inpt = document.getElementById(`searched_item`)
-
+let inpt = document.getElementsByClassName(`searched_item`)
 let cur_str = "";
+let cur_arr = [];
 
-inpt.addEventListener(`keyup`, (e) => {
-    
+inpt[0].addEventListener(`keyup`, (e) => {
     if(e.keyCode === 13){
-        let str = inpt.value
-        let data = JSON.parse(localStorage.getItem(`all_items`))
-            let found_data = []
-            for(let i = 0; i < data.length; i++){
-                if(str == data[i].name){
-                    found_data.push(data[i])
-                    displayData(found_data)
-                    break;
-                }
-            }
+        displayData(cur_arr)
+        cur_str = ""
+        cur_arr = []
     }
 
     else if(e.keyCode === 32){
-        console.log(cur_str);
+        let str = "";
+        for(let i = 0; i < cur_str.length; i++){
+            str += cur_str[i]
+        }
         cur_str = "";
+        // console.log(cur_arr)
+        if(cur_arr.length == 0){
+            let data = JSON.parse(localStorage.getItem(`all_items`))
+            for(let i = 0; i < data.length; i++){
+                let arr_str = data[i].name.split(" ")
+                
+                for(let j = 0; j < arr_str.length; j++){
+                    if(str == arr_str[j]){
+                        cur_arr.push(data[i])
+                        break
+                    }
+                }
+            }
+        }
+
+        else{
+            let arr = [];
+            for(let i = 0; i < cur_arr.length; i++){
+                arr.push(cur_arr[i])
+            }
+            cur_arr = []
+            for(let i = 0; i < arr.length; i++){
+                let new_arr_str = arr[i].name.split(" ")
+                for(let j = 0; j < new_arr_str.length; j++){
+                    if(str == new_arr_str[j]){
+                        cur_arr.push(arr[i])
+                    }
+                }
+            }
+        }
     }
     
+    else if((e.keyCode == 16) || (e.keyCode == 20)){
+       
+    }
+
     else{
-        cur_str += e;
+        cur_str += e.key;
     }
 })
 
 
 
 const displayData = (data) => {
+    console.log(data)
     let main_container = document.getElementById(`main_container`)
     main_container.style.display = "none"
+
     let popup_container = document.getElementById(`popup_container`)
     popup_container.style.display = "flex"
+
+    let popup_left = document.getElementById(`popup_left`)
+    let h1_div = document.createElement(`div`)
+    let h1 = document.createElement(`h1`)
+    h1.textContent = `${data.length} RESULTS`
+    h1_div.append(h1)
+
+    let btn_div = document.createElement(`div`)
+    let clr_srch = document.createElement(`button`)
+    clr_srch.textContent = "Clear Search"
+    btn_div.append(clr_srch)
+
+    popup_left.append(h1_div, btn_div)
+
+    let searched_food = document.getElementById(`searched_food`)
+    searched_food.style.display = "grid"
+    searched_food.style.gridTemplateColumns = "repeat(3, 1fr)"
+    searched_food.style.gridGap = "20px"
+
     data.forEach((item) => {
         let div = document.createElement(`div`)
 
@@ -53,7 +103,8 @@ const displayData = (data) => {
         p2.style.color = "gray"
 
         div.append(img, h3, p1, p2)
-        popup_container.append(div)
+        searched_food.append(div)
 
     })
 }
+
