165b5824e2d2d29ae88d82c389fc480e0c61bcbe
Manish Kumar <manishhansalmeh@gmail.com>
2022-03-06 15:58:48 +0530

    Navbar and footer added to deals.html

--- dealsNavbar.js
+++ dealsNavbar.js
@@ -1,11 +1,11 @@
 import navbar from "./components/navbar.js";
 
-let header = document.getElementById("header");
+let header = document.getElementById("dealsNavbar");
 
 header.innerHTML = navbar();
 
 let select = document.querySelector(".bucketIconDiv");
 select.addEventListener("click", move);
 function move() {
-    window.location = "cart.html";
+    window.location = "startOrder.html";
 }
